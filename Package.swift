a9799dddce223ff5272301612ded1ef5868128ac
Ivan Oparin <i.oparin@unitea.com>
2024-09-04 16:36:36 +0400

    Fix module name

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     products: [
         .library(
             name: "ReluxFeatureManagement",
-            targets: ["FeatureManagement"]
+            targets: ["ReluxFeatureManagement"]
         ),
     ],
     dependencies: [
