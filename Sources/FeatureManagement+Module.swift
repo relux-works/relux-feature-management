932bc2f5c0d9d702495978c50e348b1857800a24
Григорьев Алексей Андреевич <alexis.ag85@gmail.com>
2024-09-20 20:45:29 +0300

    updating module interface

--- Sources/FeatureManagement+Module.swift
+++ Sources/FeatureManagement+Module.swift
@@ -6,21 +6,25 @@ extension FeatureManagement {
     @MainActor
     public final class Module: Relux.Module {
         public let service: IFeatureManagementService
-        
+        public let viewState: FeatureManagement.UI.ViewState
+
         public init(
             store: IFeatureManagementStore,
             allFeatures: [FeatureManagement.Business.Model.Feature]
         ) {
-            self.service = FeatureManagement.Business.Service(store: store)
-            
             let state = FeatureManagement.Business.State()
+
             let viewState = FeatureManagement.UI.ViewState(
                 featureState: state,
                 allFeatures:  allFeatures
             )
+            self.viewState = viewState
 
-            let saga = FeatureManagement.Business.Saga(svc:service)
-            
+            let service = FeatureManagement.Business.Service(store: store)
+            self.service = service
+
+            let saga = FeatureManagement.Business.Saga(svc: service)
+
             super.init(
                 states: [state],
                 viewStates: [viewState],
