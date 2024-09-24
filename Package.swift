5c8e9743deeea1e0ae0313f9b7d4a8b923a3ffb8
Григорьев Алексей Андреевич <alexis.ag85@gmail.com>
2024-09-24 21:35:15 +0300

    updating min version

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "ios-featuremanagement",
     platforms: [
-        .iOS(.v17)
+        .iOS(.v15)
     ],
     products: [
         .library(
